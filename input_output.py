b1a59c548b8951074bce7bc8066116974920b21d
snehdeep07 <snehdeepramteke@gmail.com>
2025-06-12 15:58:31 +0530

    Create input_output.py

---
+++ input_output.py
@@ -0,0 +1,32 @@
+# assinging variable values
+# a = 18
+# b = 18.50
+# string = "snehdeep.ineuron"
+# bool = True
+# printing variable values
+# print("value of a is",a, "result done !!")
+# print("value of a is",b, "result done !!")
+# print("value of a is",string, "result done !!")
+# print("value of a is",bool ,"result done !!")
+
+#checking the data type of variable
+# we can check using type()
+# print("type of a is",type(a))
+# print("type of a is",type(b))
+# print("type of a is",type(string))
+# print("type of a is",type(bool))
+
+# how to do type casting
+# a =  a + 10
+# print(float(a))
+
+
+
+# how to take the input in python
+# we can use input() function
+name = input("enter value for name = ")
+age = int(input("enter value for age = "))
+print("username is = ",name)
+print("user age = ",age)
+print("type of name",type(name))
+print("type of age",type(age))
