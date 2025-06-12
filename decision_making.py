6358ed38e8150ad72e64f14e63dcc9e3c460939d
snehdeep07 <snehdeepramteke@gmail.com>
2025-06-12 16:00:37 +0530

    Create decision_making.py

---
+++ decision_making.py
@@ -0,0 +1,27 @@
+# if.....else statement
+# x=5
+# y=10
+# if(x<=10):
+#     print("x is smaller than y")
+# else:
+#     print("y is greter  than x")
+
+# a=20
+# b=50
+# if(a==b):
+#     print("a is equals to b")
+# print("a is not equals to b")
+
+# Nested if....else statement
+marks = 100
+if(marks>90):
+    print("grade A+")
+elif(marks>=80 and marks<90):
+    print("Grade A")
+elif(marks>=70 and marks>80):
+    print("Grade B+")
+elif(marks>=60 and marks>70):
+    print("Grade B")
+else:
+    print("failed")
+
