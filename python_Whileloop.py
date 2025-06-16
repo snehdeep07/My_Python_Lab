17ab5df249275173b9de830ca9250816358f908f
snehdeep07 <snehdeepramteke@gmail.com>
2025-06-16 13:06:34 +0530

    Create python_Whileloop.py

---
+++ python_Whileloop.py
@@ -0,0 +1,12 @@
+# While Loop
+# syntax
+# while expression:
+# statement
+num=9
+counter=1
+while counter<=10:
+    ans = num * counter
+    print(num,"*",counter,"=",ans)
+    counter = counter + 1
+while True:
+    print("iNeuron")
