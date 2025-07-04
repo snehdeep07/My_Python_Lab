8b772c1bb9e19535f1279976feb94a6579e7ca50
snehdeep07 <snehdeepramteke@gmail.com>
2025-07-04 17:49:58 +0530

    Create python_string.py

---
+++ python_string.py
@@ -0,0 +1,44 @@
+# # create a string in python
+# # string in python can be created using single quotes, double quotes, or triple quotes. 
+# # str = "snehdeep"
+# # print(str)
+
+# # str2="snehdeep 'ramteke'"
+# # print(str2)
+
+# # str3='snehdeep "ramteke"'
+# # print(str3)
+# # # to print multiline string in python
+# # str4='''snehdeep 
+# # ramteke have good kind of
+# # persona'''
+# # print(str4)
+
+# # # to calculate the length of a string
+# # print("the lenght of string is:",len(str3))
+
+# # # string concatenation
+# # str1= "snehdeep"
+# # str2="ramteke"
+# # print(str1 + str2)
+
+# # # string comparison
+# # str_1= "snehdeep"
+# str_2="snehdeep"
+# # print(str_1 == str_2)
+
+# # how to print the each character in a string
+# #  it prints all the characters present in string
+# # for char in str_2:
+# #     print(char)
+
+# # it print single character that we want to print
+# print(str_2[4])
+
+# converting the string into uppper and lower case
+str_7 = "snehdeep"
+print(str_7.upper())
+
+
+str_7 = "SNEHDEEP"
+print(str_7.lower())
