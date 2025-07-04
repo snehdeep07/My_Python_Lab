5175829632c23a19f3d22a4de736370f44def9f6
snehdeep07 <snehdeepramteke@gmail.com>
2025-07-04 17:50:27 +0530

    Create python_tuple.py

---
+++ python_tuple.py
@@ -0,0 +1,7 @@
+# A tuple is a built-in data type in Python 
+# used to store multiple items in a single variable
+t1 =(1,2,3,4,5,6)
+# t1[0]=10   tuple does not support assignment and append
+# t1.append(1) 
+print(t1[2])
+# tuple supports indexing
