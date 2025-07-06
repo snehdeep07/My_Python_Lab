f3e254d343b40dcd9629b33fa3465a72fe30a666
snehdeep07 <snehdeepramteke@gmail.com>
2025-07-06 16:49:24 +0530

    Update python_dict.py

--- python_dict.py
+++ python_dict.py
@@ -42,3 +42,33 @@ students = {
     }
 }
 print(students["student1"]["marks"]["math"])
+# print(students["student1"]["marks"]["math"])
+
+# # how to update the value in dictionary
+# students["student1"]["age"]=30
+# print(students["student1"])
+
+# dict2['other details']['nationality']='USA'
+# print(dict2['other details'])
+
+# # to print the total key in dict
+# total_key = students["student1"].keys()
+# print('total key in students:' ,total_key)
+
+# # hot to iterate the entire dictionary
+# for k,v in students.items():
+#     print("keys are =",k,"values are =",v)
+
+# # dictionary comparison
+# dict3={'a' :1, 'b': 2, 'c': 3}
+# dict4={'b': 2, 'a' : 4, 'c' :3}
+# print(dict3 == dict4)
+
+#  how to delete specific key value in dictionary
+# del students["student1"]["name"]
+# del students["student2"]["name"]
+# print(students)
+
+# to check wether specific key exist in dictionary or not
+# dict_name.has_key()
+print(students["student1"]["name"].has_key())
